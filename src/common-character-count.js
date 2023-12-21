69671f749a1c0334d9d8cebc6e328d7f62c17ae6
Snischuk Maxim <m.snischuk@gmail.com>
2023-12-21 07:14:55 +0200

    feat: add tasks mine-sweeper, mac-address, sort by height and common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const charCount = new Map();
+
+  for (const char of s1) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  let commonCount = 0;
+
+  for (const char of s2) {
+    if (charCount.get(char) > 0) {
+      commonCount++;
+      charCount.set(char, charCount.get(char) - 1);
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +23,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rowsCount = matrix.length;
+  const colsCount = matrix[0].length;
+
+  function isValidCell(row, col) {
+    return row >= 0 && row < rowsCount && col >= 0 && col < colsCount;
+  }
+
+  function countNeighbors(row, col) {
+    let mineCount = 0;
+    const minRow = Math.max(0, row - 1);
+    const maxRow = Math.min(row + 1, rowsCount - 1);
+    const minCol = Math.max(0, col - 1);
+    const maxCol = Math.min(col + 1, colsCount - 1);
+
+    for (let i = minRow; i <= maxRow; i++) {
+      for (let j = minCol; j <= maxCol; j++) {
+        if (i !== row || j !== col) {
+          if (isValidCell(i, j) && matrix[i][j]) {
+            mineCount++;
+          }
+        }
+      }
+    }
+
+    return mineCount;
+  }
+
+  return matrix.map((row, i) => row.map((cell, j) => {
+    return cell ? 1 : (0 + countNeighbors(i, j));
+  }));
 }
 
 module.exports = {
