5d3b564e3d1ed5c467fd755b54283eb3401003ac
Max Snischuk <m.snischuk@gmail.com>
2025-10-24 03:04:17 +0300

    feat: add minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+// const { NotImplementedError } = require('../lib');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +23,37 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const rowsCount = matrix.length;
+  const colsCount = matrix[0].length;
+
+  function isValidCell(row, col) {
+    return row >= 0 && row < rowsCount && col >= 0 && col < colsCount;
+  }
+
+  function countNeighbors(row, col) {
+    let mineCount = 0;
+    const minRow = Math.max(0, row - 1);
+    const maxRow = Math.min(row + 1, rowsCount - 1);
+    const minCol = Math.max(0, col - 1);
+    const maxCol = Math.min(col + 1, colsCount - 1);
+
+    for (let i = minRow; i <= maxRow; i++) {
+      for (let j = minCol; j <= maxCol; j++) {
+        if (i !== row || j !== col) {
+          if (isValidCell(i, j) && matrix[i][j]) {
+            mineCount++;
+          }
+        }
+      }
+    }
+
+    return mineCount;
+  }
+
+  return matrix.map((row, i) => row.map((cell, j) => {
+    return cell ? 1 : (0 + countNeighbors(i, j));
+  }));
 }
 
 module.exports = {
